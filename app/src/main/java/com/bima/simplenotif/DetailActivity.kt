d18f7fa0689615f28de95e69d7592eadfb94dc68
bimamib <bimaprast1601@gmail.com>
2023-09-25 02:00:41 +0700

    membuat kode untuk menerima data dari Intent

--- app/src/main/java/com/bima/simplenotif/DetailActivity.kt
+++ app/src/main/java/com/bima/simplenotif/DetailActivity.kt
@@ -2,10 +2,26 @@ package com.bima.simplenotif
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bima.simplenotif.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        binding.tvTitle.text = title
+        binding.tvMessage.text = message
+    }
+
+    companion object {
+        const val EXTRA_TITLE = "extra_title"
+        const val EXTRA_MESSAGE = "extra_message"
     }
 }
